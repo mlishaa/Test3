526cf3866e989f3653c3512faa95fa8a2ac096be
mac <mac@DESKTOP-T73Q40O>
2019-12-08 19:19:02 -0500

    withSecondMigration

---
+++ Test3/Migrations/201912090017538_MySecond.cs
@@ -0,0 +1,16 @@
+﻿namespace Test3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MySecond : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
